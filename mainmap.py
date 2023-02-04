752108e5d5bd55669a1c788886b2d2c6025c7f58
jb69535 <92282867+jb69535@users.noreply.github.com>
2023-02-04 00:03:22 -0500

    mainmap.py changes

--- mainmap.py
+++ mainmap.py
@@ -1,24 +1,44 @@
+import pandas as pd 
 import folium
 import branca
-import pandas as pd
 import numpy as np
 import plotly.express as px
-import plotly.graph_objects as go
 
-df = pd.read_excel('TokyoCafe.xlxs')
-df
-location = df['latitude'].mean(), df['longitude'].mean()
-m = folium.Map(location=location,zoom_start=3)
-m
+
+import webbrowser
+class Map:
+    def init(self, center, zoom_start):
+        self.center = center
+        self.zoom_start = zoom_start
+
+    def showMap(self):
+        #Create the map
+        my_map = folium.Map(location = self.center, zoom_start = self.zoom_start)
+        #Display the map
+        my_map.save("map.html")
+        webbrowser.open("map.html")
+
+
+#Define coordinates of where we want to center our map
+coords = [35.7036892655866, 139.66090693994255]
+map = Map(center = coords, zoom_start = 13)
+map.showMap()
+
+# import plotly.graph_objects as go
+
+df = pd.read_excel('TokyoCafe.xlsx')
+print(df)
+
 
 # code making chart
-#df_temp=pd.DataFrame
-#for index in :
-#    df['Study'] =
+df_temp = pd.DataFrame
+
+#for i in range(df.index):
+ #   df['Study'] =
 #   df['Date'] = 
 #   df['Chill'] = 
 
-    
+
  #   df_temp= pd.append(figure)
 #return df
 
