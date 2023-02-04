1ccd6cd645774539711a7f9017042cdeaf58e721
jb69535 <92282867+jb69535@users.noreply.github.com>
2023-02-04 15:44:01 -0500

    main

--- main.py
+++ main.py
@@ -29,14 +29,13 @@ def get_location(ip):
 
 location = get_location('ip')
 m = folium.Map(location=location,zoom_start=13)
-m
 
 
 def auto_open(path):
     html_page = f'{path}'
     f_map.save(html_page)
     # open in browser.
-    new = 2
+    new=2
     webbrowser.open(html_page, new=new)
 
 df=pd.read_excel('TokyoCafe.xlsx')
@@ -77,8 +76,8 @@ def popup_html(row):
 
 for i in range(0, len(df)):
     html = popup_html(i)
-    iframe = branca.element.IFrame(html=html, width=510, height=280)
-    popup = folium.Popup(folium.Html(html, script=True), max_width=500)
+    iframe = branca.element.IFrame(html=html, width=900, height=700)
+    popup = folium.Popup(folium.Html(html, script=True), max_width=500, min_width=500)
     folium.Marker([df['latitude'].iloc[i], df['longitude'].iloc[i]],
                   popup=popup, icon=folium.Icon(color='blue', icon='info-sign', prefix='fa')).add_to(m)
 
